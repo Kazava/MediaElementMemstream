1fbc00a30d14949b449834c10e373c79c9fd3473
jasells <joshua.sells@gmail.com>
2016-10-26 12:59:30 -0500

    added async error handler

---
+++ MediaElementMemstream/AsyncErrorHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaElementMemstream
+{
+    public static class AsyncErrorHandler
+    {
+        public static void HandleException(Exception ex)
+        {
+            if (ex != null)
+            {
+
+                System.Diagnostics.Debug.WriteLine("== Error in task in MediaElementMemstream ==");
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(ex.StackTrace);
+                HandleException(ex.InnerException);
+            }
+        }
+    }
+}
